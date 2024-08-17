c4f5147fadbb3fb833bd73dc6bf9cfead5f50fab
Hyk07 <theorder.cp@gmail.com>
2024-08-17 16:51:11 +0300

    v1
    gerekli kitap, üye eklemeleri/düzenlemeleri, teslim işlemleri, kitap sorgulama işlemleri csv dosyaları ile sağlanmaktadır

---
+++ KutuphaneOtoGP/Login.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace KutuphaneOtoGP
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            label1 = new Label();
+            label2 = new Label();
+            nameTXT = new TextBox();
+            passTXT = new TextBox();
+            button1 = new Button();
+            linkLabel1 = new LinkLabel();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(459, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(95, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Kullanıcı Adı:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(515, 54);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 20);
+            label2.TabIndex = 0;
+            label2.Text = "Şifre";
+            // 
+            // nameTXT
+            // 
+            nameTXT.Location = new Point(560, 22);
+            nameTXT.Name = "nameTXT";
+            nameTXT.Size = new Size(163, 27);
+            nameTXT.TabIndex = 1;
+            // 
+            // passTXT
+            // 
+            passTXT.Location = new Point(560, 55);
+            passTXT.Name = "passTXT";
+            passTXT.Size = new Size(163, 27);
+            passTXT.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.Location = new Point(624, 118);
+            button1.Name = "button1";
+            button1.Size = new Size(99, 56);
+            button1.TabIndex = 2;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(597, 87);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(126, 20);
+            linkLabel1.TabIndex = 3;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Hesabın  yok mu?";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-2, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(455, 297);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Highlight;
+            ClientSize = new Size(735, 298);
+            Controls.Add(pictureBox1);
+            Controls.Add(linkLabel1);
+            Controls.Add(button1);
+            Controls.Add(passTXT);
+            Controls.Add(nameTXT);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Login";
+            Text = "Giriş Yap";
+            Load += Login_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox nameTXT;
+        private TextBox passTXT;
+        private Button button1;
+        private LinkLabel linkLabel1;
+        private PictureBox pictureBox1;
+    }
+}
